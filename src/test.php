db009c4c64e8fd3d5aaa6f3f21b89366ed3e0a31
Andrea Faulds <ajf@ajf.me>
2015-01-20 16:37:42 +0000

    Import more unit tests from GG2

--- src/test.php
+++ src/test.php
@@ -31,4 +31,101 @@ class Tests extends \PHPUnit_Framework_TestCase
             ['foo' => 'bar', 'baz' => 'qux']
         );
     }
+
+    public function testListDecode() {
+        $this->assertEquals(
+            Parser::parse('[]'),
+            ['length' => '0']
+        );
+
+        $this->assertEquals(
+            Parser::parse('[a, b]'),
+            ['length' => '2', '0' => 'a', '1' => 'b']
+        );
+    }
+
+    public function testMixed() {
+        // contrived, but probably semi-representative sample
+        $testCase = "{
+    items: [
+        {
+            type: foo,
+            x: 12.0,
+            y: 13.5
+        },
+        {
+            type: bar,
+            x: 12.0,
+            y: 13.5,
+            'sub items': []
+        }
+    ]
+}";
+
+        $parsed = Parser::parse($testCase);
+
+        $this->assertEquals(
+            $parsed,
+            [
+                "items" => [
+                    [
+                        "type" => "foo",
+                        "x" => "12.0",
+                        "y" => "13.5"
+                    ],
+                    [
+                        "type" => "bar",
+                        "x" => "12.0",
+                        "y" => "13.5",
+                        "sub items" => [
+                            "length" => 0
+                        ]
+                    ]
+                ]
+            ]
+        );
+
+        $this->assertEquals(
+            Encoder::encode($parsed),
+             "{items:[{type:foo,x:12.0,y:13.5},{type:bar,x:12.0,y:13.5,'sub items':[]}]}"
+        );
+    }
+
+    public function testListEncode() {
+        // valid empty GGON list
+        $this->assertEquals(
+            Encoder::encode(["length" => "0"]),
+            "[]"
+        );
+
+        // valid GGON list
+        $this->assertEquals(
+            Encoder::encode(["length" => "2", "0" => "a", "1" => "b"]),
+            "[a,b]"
+        );
+
+        $encoded = Encoder::encode(["length" => "a", "0" => "a", "1" => "b"]);
+
+        // length is invalid, not valid GGON list
+        $this->assertEquals(
+            $encoded[0],
+            "{"
+        );
+
+        $encoded = Encoder::encode(["length" => "3", "0" => "a", "1" => "b"]);
+
+        // length is wrong, also not valid GGON list
+        $this->assertEquals(
+            $encoded[0],
+            "{"
+        );
+        
+        $encoded = Encoder::encode(["length" => "3", "0" => "a", "2" => "c"]);
+
+        // sparse, not a valid GGON list
+        $this->assertEquals(
+            $encoded[0],
+            "{"
+        );
+    }
 }
