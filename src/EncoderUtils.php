5155e5ca96e246d411e951c9a14221cfa67cff4c
Andrea Faulds <ajf@ajf.me>
2015-01-30 15:37:29 +0000

    Update listToMap/mapToList docs

--- src/EncoderUtils.php
+++ src/EncoderUtils.php
@@ -5,7 +5,9 @@ class EncoderUtils
 {
     // Takes an array
     // Returns an array which has a "length" key and a string key for each index
-    // This is a function to make dealing with GGON easier, as GGON lacks lists
+    // This is a function to make dealing with GGON easier, as GGON doesn't
+    // automatically produce lists from numeric PHP arrays
+    // The array produced has the same format as a decoded a GGON list
     // This is equivalent to the GML function ggon_list_to_map
     public static function listToMap(array $list)
     {
@@ -21,8 +23,10 @@ class EncoderUtils
     
     // Takes an array which has a "length" key and string indexes up to length
     // Returns a numerically-indexed array which has the values of those indexes in order
-    // This is a function to make dealing with GGON easier, as GGON lacks lists
-    // This is designed to work with the array ggon_list_to_map/listToMap would produce
+    // This is a function to make dealing with GGON easier, as GGON doesn't
+    // automatically produce numeric PHP arrays from GGON lists
+    // This is designed to work with what decoding GGON lists, ggon_list_to_map
+    // or listToMap would produce
     // This is equivalent to the GML function ggon_map_to_list
     public static function mapToList(array $map)
     {
